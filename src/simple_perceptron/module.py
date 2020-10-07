7f8a01bd9e8244d3be2ae65a235eb0313f791f59
Karol Belina <karolbelina@gmail.com>
2020-10-07 15:58:31 +0200

    Fix the training procedure of the simple perceptron

--- src/simple_perceptron/module.py
+++ src/simple_perceptron/module.py
@@ -4,24 +4,30 @@ import numpy as np
 class SimplePerceptron:
     def __init__(self, size, learning_rate, threshold=None, logic='bipolar'):
         self.learning_rate = learning_rate
-        self.weights = np.random.standard_normal(size)
+        self.weights = np.random.uniform(low=-1.0, high=1.0, size=size)
         self.bias = threshold
         if logic in ['unipolar', 'bipolar']:
             self.logic = logic
         else:
             raise ValueError(f"'{logic}' is not a valid logic type")
 
-    def predict(self, a):
-        return self.activation(np.dot(a, self.weights) + self.bias)
+    def fit(self, X, y):
+        while True:
+            error = False
+            for x, d in zip(X, y):
+                z = np.dot(x, self.weights) + self.bias
+                a = self._activation(z)
+                delta = d - a
+                if delta != 0:
+                    error = True
+                    self.weights = self.weights + self.learning_rate * delta * x
+            if not error:
+                return
 
-    def train(self, D):
-        for x, d in D:
-            z = np.dot(x, self.weights) + self.bias
-            a = self.activation(z)
-            delta = d - a
-            self.weights += self.learning_rate * delta * self.weights 
+    def predict(self, X):
+        return self._activation(np.dot(X, self.weights) + self.bias)
 
-    def activation(self, x):
+    def _activation(self, x):
         return 1 if x > 0 else (-1 if self.logic == 'bipolar' else 0)
     
     def __repr__(self):

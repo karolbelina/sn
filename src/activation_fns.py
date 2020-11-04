Fix the softmax and cross entropy functions

src/activation_fns.py
@@ -44,11 +44,8 @@ class Softmax(ActivationFunction):
         # for numerical stability make the maximum of x to be 0
         stable_x = x - x.max(axis=1)[:, None]
         exp_x = np.exp(stable_x)
-        soft = exp_x / exp_x.sum(axis=1)[:, None]
-        
-        self.cache = soft
 
-        return soft
+        return exp_x / exp_x.sum(axis=1)[:, None]
     
     def derivative(self, x: np.ndarray) -> np.ndarray:
-        return self.cache * (x - (x * self.cache).sum(axis=1)[:, None])
+        return np.ones_like(x)

src/multilayer_perceptron/trainers.py
@@ -35,7 +35,7 @@ class Trainer(AbstractTrainer):
 
         return stable_output - label
 
-    def fit(self, model: MultilayerPerceptron) -> None:
+    def fit(self, model: MultilayerPerceptron) -> float:
         for x, y in self._train_dataloader.get_batches():
             y_hat = model(x)
             da = self._cost_fn_backward(y_hat, y)
@@ -51,13 +51,19 @@ class Trainer(AbstractTrainer):
     def _validate(self, model: Model, val_batch: tuple[np.ndarray, np.ndarray]) -> float:
         x, y_hat = val_batch
         y = model(x)
-        error = self._cost_fn(y, y_hat)
+        
+        loss = self._cost_fn(y, y_hat)
 
         result_classes = y_hat.argmax(axis=1)
         label_classes = y.argmax(axis=1)
-        acc = (result_classes == label_classes).mean()
 
-        print(f"loss = {error}, acc = {acc * 100}%")
+        Nc = (result_classes == label_classes).sum()
+        Nt = len(result_classes)
+        
+        accuracy = Nc / Nt
+        error = (Nt - Nc) / Nt
+
+        print(f"loss = {loss:.2f}, acc = {accuracy:.2%}")
 
         return error
     

src/supervisor.py
@@ -4,24 +4,58 @@ from trainer import Trainer
 from typing import Optional
 
 import numpy as np
+import pickle as pkl
 
 
 class Supervisor:
-    def __init__(self, max_epochs: Optional[int] = None, epsilon: float = 0) -> None:
+    def __init__(
+        self,
+        model: Model,
+        trainer: Trainer,
+        max_epochs: Optional[int] = None,
+        epsilon: float = 0
+    ) -> None:
         self._max_epochs = max_epochs if max_epochs is not None else np.inf
         self._epsilon = epsilon
 
-    def run(
-        self,
-        model: Model,
-        trainer: Trainer
-    ) -> None:
-        val_error = None
-        epoch_number = 0
+        self._error = None
+        self._epoch_number = 0
+        
+        self._model = model
+        self._trainer = trainer
 
-        while epoch_number < self._max_epochs and (val_error is None or val_error > self._epsilon):
-            epoch_number += 1
+    def run(self) -> int:
+        try:
+            while self._epoch_number < self._max_epochs:
+                self._epoch_number += 1
 
-            val_error = trainer.fit(model)
+                self._error = self._trainer.fit(self._model)
 
-        return epoch_number
+                if self._error <= self._epsilon:
+                    print("Achieved the derired error threshold")
+                    self.pause()
+                    break
+
+            return self._epoch_number
+        except KeyboardInterrupt:
+           self.pause()
+    
+    def pause(self) -> None:
+        while True:
+            try:
+                decision = input("\nDo you want to save the state? (y/N): ")
+                if decision.strip() == 'y':
+                    path = input("Please enter the file path: ")
+                    with open(path, 'wb') as file:
+                        pkl.dump(self, file)
+                        print(f"Saved the state in {path}")
+                    return
+                elif decision.strip() == 'N':
+                    return
+            except KeyboardInterrupt:
+                return
+           
+    @staticmethod
+    def resume(filename: str):
+        with open(filename, 'rb') as file:
+            return pkl.load(file)
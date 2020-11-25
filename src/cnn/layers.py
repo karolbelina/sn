Fix the final softmax layer

src/cnn/layers.py
@@ -90,9 +90,7 @@ class FullyConnectedLayer(Layer):
 
 
 class ActivationFunction(Layer):
-    def __init__(self, size: int) -> None:
-        self._size = size
-
+    def __init__(self) -> None:
         self._z = None
 
     def initialize_θ(self, weight_initializer: WeightInitializer) -> np.ndarray:
@@ -126,8 +124,8 @@ class ActivationFunction(Layer):
 
 
 class Sigmoid(ActivationFunction):
-    def __init__(self, size: int) -> None:
-        super().__init__(size)
+    def __init__(self) -> None:
+        super().__init__()
     
     def __call__(self, x: np.ndarray) -> np.ndarray:
         return 1 / (1 + np.exp(-x))

src/cnn/module.py
@@ -44,7 +44,9 @@ class ConvolutionalNeuralNetwork(Model):
         for layer in self._layers:
             x = layer.feedforward(x)
         
-        return x
+        y_hat = ConvolutionalNeuralNetwork.softmax(x)
+        
+        return y_hat
     
     def dC_dθ(self, x: np.ndarray, y: np.ndarray) -> callable:
         m = x.shape[0]